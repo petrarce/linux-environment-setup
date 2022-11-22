c7138d8bbf134b5c3e90d9f607db0d0c86c6e0eb
Iohannes Folbort <ifolbort@streitberg.bvd.indurad.x>
2022-11-22 14:20:19 +0100

    modify bash_aliases

--- install_environment.sh
+++ install_environment.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-TARGET_DIR=/tmp
+set -e
+set -x
+TARGET_DIR=${HOME}
 
-for i in environment/*; do 
-	echo $i; 
-	if [ -f  /tmp/.$i ]; then
-		mv /tmp/.$i /tmp/.$i.old
-		ln -s ${PWD}/environment/$i  ${TARGET_DIR}/.$i
+LOCALDIR=`pwd`
+
+for CONFIG_FILE_PATH in environment/*; do
+
+	FONCIF_FILE=`basename ${CONFIG_FILE_PATH}`
+	pushd ${TARGET_DIR}
+	pwd
+	if [ -f  .${FONCIF_FILE} ]; then
+		mv .${FONCIF_FILE} .${FONCIF_FILE}.old
 	fi
+	ln -sf ${LOCALDIR}/${CONFIG_FILE_PATH}  .${FONCIF_FILE}
+	popd
+
 done
+
+set +x
