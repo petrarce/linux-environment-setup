5d662543b8551bdfcecaa78a864665ae7e428fa3
petrarce <petrarce@gmail.com>
2022-11-22 15:50:01 +0100

    add tmux configuration

--- install_environment.sh
+++ install_environment.sh
@@ -2,19 +2,31 @@
 
 set -e
 set -x
-TARGET_DIR=${HOME}
 
-LOCALDIR=`pwd`
+TARGET_DIR=${HOME}
+CURRENT_DATE=`date +%Y-%m-%d-%H:%M:%S`
+LOCAL_DIR=`pwd`
+
+if [ -d "${1}" ]; then
+	TARGET_DIR=${1}
+fi
+
+if [ -d ${TARGET_DIR}/environment ]; then
+	mv ${TARGET_DIR}/environment ${TARGET_DIR}/environment.${CURRENT_DATE}.old
+fi
+install -d ${TARGET_DIR}/environment
+install -m 700 ${LOCAL_DIR}/environment/* ${TARGET_DIR}/environment
+
 
 for CONFIG_FILE_PATH in environment/*; do
 
-	FONCIF_FILE=`basename ${CONFIG_FILE_PATH}`
+	CONFIG_FILE=`basename ${CONFIG_FILE_PATH}`
 	pushd ${TARGET_DIR}
 	pwd
-	if [ -f  .${FONCIF_FILE} ]; then
-		mv .${FONCIF_FILE} .${FONCIF_FILE}.old
+	if [ -f  .${CONFIG_FILE} ]; then
+		mv .${CONFIG_FILE} .${CONFIG_FILE}.${CURRENT_DATE}.old
 	fi
-	ln -sf ${LOCALDIR}/${CONFIG_FILE_PATH}  .${FONCIF_FILE}
+	ln -sf ${TARGET_DIR}/environment/${CONFIG_FILE}  .${CONFIG_FILE}
 	popd
 
 done
